d587521d672fbc83b18859d800b041915c30a01f
Wassim-Alouini <wassimalouini6@gmail.com>
2019-10-12 17:00:16 +0200

    Updates

---
+++ precache-manifest.cbda8d02be87ec127555acd0d5aae175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ef68ddae1e50883a589fbba1c716579",
+    "url": "/-2--Checkpoint-2/index.html"
+  },
+  {
+    "revision": "620d0ff101ba7e5c00d3",
+    "url": "/-2--Checkpoint-2/static/css/main.f188ed9e.chunk.css"
+  },
+  {
+    "revision": "8a63d0c69e9ab9b8b70f",
+    "url": "/-2--Checkpoint-2/static/js/2.1632cf1e.chunk.js"
+  },
+  {
+    "revision": "620d0ff101ba7e5c00d3",
+    "url": "/-2--Checkpoint-2/static/js/main.f77b6dcf.chunk.js"
+  },
+  {
+    "revision": "23788682b788fe198220",
+    "url": "/-2--Checkpoint-2/static/js/runtime-main.75f637cf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/-2--Checkpoint-2/static/media/logo.25bf045c.svg"
+  }
+]);
